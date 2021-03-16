141943bc01be57db7f2a6b97b3fe3581abc35a23
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2021-03-16 20:52:58 -0300

    feat: add DownloadModal

--- src/components/DownloadModal.tsx
+++ src/components/DownloadModal.tsx
@@ -1,6 +1,23 @@
-import React from 'react'
-import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Button } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  Button,
+  Flex,
+  Image,
+  Text,
+  Icon,
+  HStack
+} from '@chakra-ui/react'
 import { Video } from 'youtube-sr'
+import { FaVolumeMute, FaVolumeUp, FaVideoSlash, FaVideo } from 'react-icons/fa'
+
+import Dropdown from './Dropdown'
 
 interface DownloadModalProps {
   video: Video
@@ -9,21 +26,85 @@ interface DownloadModalProps {
 }
 
 const DownloadModal: React.FC<DownloadModalProps> = ({ video, isOpen, onClose }) => {
+  const [selected, setSelected] = useState(0)
+
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
+    <Modal isOpen={isOpen} onClose={onClose} isCentered size="lg">
       <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>Modal Title</ModalHeader>
+      <ModalContent background="gray.900" color="gray.100">
+        <ModalHeader>Download Video</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          Lorem ipsum
+          <Flex direction="column">
+            <Image
+              src={video.thumbnail.url!}
+              fit="cover"
+              bgColor="gray.300"
+              borderRadius="md"
+            />
+          </Flex>
+
+          <Text
+            flex="1"
+            isTruncated noOfLines={1}
+            fontWeight="bold"
+            marginTop="2"
+            textAlign="center"
+          >
+            {video.title}
+          </Text>
         </ModalBody>
 
-        <ModalFooter>
-          <Button colorScheme="blue" mr={3} onClick={onClose}>
-            Close
-          </Button>
-          <Button variant="ghost">Secondary Action</Button>
+        <ModalFooter justifyContent="space-between">
+          <Dropdown
+            w="180px"
+            colorScheme="red"
+            selected={selected}
+            setSelected={setSelected}
+            isLoading={false}
+          >
+
+            <Flex direction="row" justify="space-between">
+              <Text>320p mp3</Text>
+              <div>
+                <Icon as={FaVideoSlash} marginRight="2" />
+                <Icon as={FaVolumeUp} />
+              </div>
+            </Flex>
+
+            <Flex direction="row" justify="space-between">
+              <Text>720p mp4</Text>
+              <div>
+                <Icon as={FaVideo} marginRight="2" />
+                <Icon as={FaVolumeMute} />
+              </div>
+            </Flex>
+
+            <Flex direction="row" justify="space-between">
+              <Text>1080p mp4</Text>
+              <div>
+                <Icon as={FaVideo} marginRight="2" />
+                <Icon as={FaVolumeUp} />
+              </div>
+            </Flex>
+          </Dropdown>
+
+          <div>
+            <Button colorScheme="red" mr={3}>
+              Download
+            </Button>
+
+            <Button
+              onClick={onClose}
+              variant="ghost"
+              _hover={{
+                background: 'gray.100',
+                color: 'gray.700'
+              }}
+            >
+              Cancel
+            </Button>
+          </div>
         </ModalFooter>
       </ModalContent>
     </Modal>
