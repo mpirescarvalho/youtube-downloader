c7b2545b52061564af481109ecaecc8b5e207ad2
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2021-03-25 22:20:55 -0300

    feat: move downloading videos to top in main list

--- src/contexts/videos.tsx
+++ src/contexts/videos.tsx
@@ -1,7 +1,8 @@
-import React, { createContext, useContext, useState } from 'react'
+import React, { createContext, useContext, useMemo, useState } from 'react'
 import { Video } from 'youtube-sr'
 
 import LoadingState from '../types/LoadingState'
+import { useDownloadingVideos } from './download'
 
 type Videos = LoadingState<Video[]>
 
@@ -13,13 +14,28 @@ type VideosData = {
 const VideosContext = createContext<VideosData>({} as VideosData)
 
 export const VideosProvider: React.FC = ({ children }) => {
-  const [videos, setVideos] = useState<Videos>({
+  const downloadingVideos = useDownloadingVideos()
+  const [rawVideos, setRawVideos] = useState<Videos>({
     data: [],
     loading: false
   })
 
+  const videos = useMemo<Videos>(() => {
+    const downloadingVideosHash = downloadingVideos.reduce((prev, curr) => {
+      prev[curr.id!] = curr
+      return prev
+    }, {} as Record<string, Video>)
+    return {
+      loading: rawVideos.loading,
+      data: [
+        ...downloadingVideos,
+        ...(rawVideos.data.filter(video => !downloadingVideosHash[video.id!]))
+      ]
+    }
+  }, [downloadingVideos, rawVideos])
+
   return (
-    <VideosContext.Provider value={{ videos, setVideos }}>
+    <VideosContext.Provider value={{ videos, setVideos: setRawVideos }}>
       {children}
     </VideosContext.Provider>
   )
